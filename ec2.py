36262bc3523bb851e01171bc106ff0e25b1aba90
tmbothe <mbothe7@hotmail.com>
2021-03-01 19:11:27 -0500

    adding more param

--- ec2.py
+++ ec2.py
@@ -1,35 +1,31 @@
 
 RDS_SECURITY_GROUP_NAME = "my-rds-public-sg"
 
+
 class EC2:
-    def __init__(self,client):
+    def __init__(self, client):
         self._client = client
 
     def create_security_group(self):
-        print(f'creating RDS Security group {RDS_SECURITY_GROUP_NAME}' )
+        print(f'creating RDS Security group {RDS_SECURITY_GROUP_NAME}')
         return self._client.create_security_group(
-            GroupName = RDS_SECURITY_GROUP_NAME,
-            Description = 'RDS security group for postgres dd',
-            VpcId = 'vpc-6b26b613'
+            GroupName=RDS_SECURITY_GROUP_NAME,
+            Description='RDS security group for postgres dd',
+            VpcId='vpc-6b26b613'
         )
 
-
-    def add_inbound_rule_to_sg(self,security_group_id):
-        print(f'Adding inbound access rule to Sg {security_group_id}' )
+    def add_inbound_rule_to_sg(self, security_group_id):
+        print(f'Adding inbound access rule to Sg {security_group_id}')
         self._client.authorize_security_group_ingress(
-            Group = security_group_id,
-            IpPermissions = [
-               {
-                'IpProtocol': 'tpc',
-                'FromPort':5432,
-                'ToPort':5432,
-                'IpRanges':[{'CidrIp': '0.0.0.0/0'}]
+            GroupId=security_group_id,
+            IpPermissions=[
+                {
+                    'IpProtocol': 'tcp',
+                    'FromPort': 5432,
+                    'ToPort': 5432,
+                    'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
 
-            }
+                }
 
             ]
         )
-
-
-        
-        
--- rds.py
+++ rds.py
@@ -3,10 +3,11 @@ from client_factory import ECSClient
 from ec2 import EC2
 
 
-RDS_DB_SUBNET_NAME ='my-rds-subnet-group'
+RDS_DB_SUBNET_NAME = 'my-rds-subnet-group'
+
 
 class RDS:
-    def __init__(self,client):
+    def __init__(self, client):
         self._client = client
         """ :type :pyboto3.rds   """
 
@@ -15,58 +16,57 @@ class RDS:
 
         security_group_id = self.create_db_security_group_and_rules()
 
-        #create subnet group
+        # create subnet group
         self.create_db_subnet_group()
         print('Subnet group created')
-        
+
         self._client.create_db_instance(
-            DBName = 'MyPostgresSQLDB',
+            DBName='MyPostgresSQLDB',
             DBInstanceIdentifier='mypostgresdb',
             DBInstanceClass='db.t2.micro',
-            Engine = 'postgres',
+            Engine='postgres',
             EngineVersion='9.6.6',
-            Port = 5432,
+            Port=5432,
             MasterUsername='postgres',
-            masterUserPassword='mypostgrespassword',
-            Allocatestorage = 20,
+            MasterUserPassword='mypostgrespassword',
+            AllocatedStorage=20,
             MultiAZ=False,
             StorageType='gp2',
             PubliclyAccessible=True,
             VpcSecurityGroupIds=[security_group_id],
-            DBSubnetGroupName = RDS_DB_SUBNET_NAME,
-            Tags = [
+            DBSubnetGroupName=RDS_DB_SUBNET_NAME,
+            Tags=[
                 {
-                    'Key' : 'Name',
-                    'Value':'Thim'
+                    'Key': 'Name',
+                    'Value': 'Thim'
                 }
             ]
-        )  
+        )
 
     def create_db_subnet_group(self):
         print('created RDS subnet group {RDS_DB_SUBNET_NAME}')
         self._client.create_db_subnet_group(
             DBSubnetGroupName=RDS_DB_SUBNET_NAME,
             DBSubnetGroupDescription='My own subnet group for RDS DB',
-            SubnetIds = ['subnet-e65324cd','subnet-5e282615','subnet-ebb847b6','subnet-0902ef71']
+            SubnetIds=['subnet-e65324cd', 'subnet-5e282615',
+                       'subnet-ebb847b6', 'subnet-0902ef71']
         )
 
     def create_db_security_group_and_rules(self):
         ec2_client = ECSClient().get_client()
         ec2 = EC2(ec2_client)
 
-        #Security group
+        # Security group
         security_group = ec2.create_security_group()
 
-        #get id of sg
+        # get id of sg
         security_group_id = security_group['GroupId']
 
         print(f"created RD with security group {security_group_id}")
 
-        #add public access rule to sg
+        # add public access rule to sg
         ec2.add_inbound_rule_to_sg(security_group_id)
-        print(f'Added inbound public access rule to sg with id ={security_group_id}')
+        print(
+            f'Added inbound public access rule to sg with id ={security_group_id}')
 
         return security_group_id
-
-
-
